54eeeb9ba17cbef8a692e10120dea7ded71322f5
georgedimitriadis <g.dimitri@hotmail.com>
2018-04-25 15:44:20 +0100

    Added code from themeaningofbrain

---
+++ gpu.py
@@ -0,0 +1,309 @@
+
+import numpy as np
+from numba import cuda
+import pyculib.blas as cublas
+import pyculib.sorting as sorting
+import numba
+from numba import float32, guvectorize
+import math
+from timeit import default_timer as timer
+from . import stopwatch as sw
+
+THREADS_PER_BLOCK = 32
+
+
+@guvectorize([(float32[:, :], float32[:])], '(m,k)->(m)', nopython=True)
+def _create_dot_product(a, dots_a):
+    """
+    Creates the dot product a[i] . a[i] for each row of the matrix and returns the vector of products
+
+    :param a: The matrix
+    :type a: float32[:,:]
+    :param dots_a: The resulting vector of dot product squares
+    :type dots_a: float32[:]
+    """
+    for i in np.arange(a.shape[0]):
+        dots_a[i] = np.dot(a[i, :], a[i, :])
+
+
+@cuda.jit
+def _sums_of_dots_gpu(dots_a, dots_b, s_o_dots):
+    """
+    GPU kernel function creating the sums of all values of dots_a vector with all values of dots_b vector
+
+    :param dots_a: The first vector
+    :type dots_a: device float32[:]
+    :param dots_b: The second vector
+    :type dots_b: device float32[:]
+    :param s_o_dots: The resulting sums
+    :type s_o_dots: device float32[:,:]
+    """
+    a_index = cuda.threadIdx.x + cuda.blockDim.x * cuda.blockIdx.x
+
+    if a_index < dots_a.shape[0]:
+        for b_index in range(dots_b.shape[0]):
+            s_o_dots[a_index, b_index] = dots_a[a_index] + dots_b[b_index]
+
+
+def _get_inner_products(a, b, verbose):
+    """
+    Runs the _create_dot_product function on two matrices a and b and times itself
+
+    :param a: The first matrix
+    :type a: float32[:, :]
+    :param b: The second matrix
+    :type b: flaot32[:, :]
+    :param verbose: If true print the time the function took to complete
+    :type verbose: bool
+    :return: The resulting vectors
+    :rtype: (float32[:], float32[:])
+    """
+    with sw.Stopwatch() as stopwatch:
+        dots_a = _create_dot_product(a)
+        dots_b = _create_dot_product(b)
+
+    if verbose:
+        print("Making the dot products Time: {0:.3f} s".format(stopwatch.total_run_time))
+
+    return dots_a, dots_b
+
+
+def _get_sum_of_dot_products(dots_a, dots_b, distances_on_gpu, verbose):
+    """
+    Put the dot products on the gpu, calculate the ||a||^2 + ||b||^2 sum of dot products matrix (a.a + b.b) (on the gpu)
+    and time the whole function
+
+    :param dots_a: The first vector
+    :type dots_a: float32[:]
+    :param dots_b: The second vector
+    :type dots_b: float32[:]
+    :param distances_on_gpu: The resulting per element sums matrix
+    :type distances_on_gpu: device float32[:, :]
+    :param verbose: If true print the time required to complete the function
+    :type verbose: bool
+    """
+    with sw.Stopwatch() as stopwatch:
+        ddots_a = cuda.to_device(np.asfortranarray(dots_a))
+        ddots_b = cuda.to_device(np.asfortranarray(dots_b))
+
+        threads_per_block = THREADS_PER_BLOCK
+        blocks_per_grid = math.ceil(ddots_a.shape[0] / threads_per_block)
+        _sums_of_dots_gpu[blocks_per_grid, threads_per_block](ddots_a, ddots_b, distances_on_gpu)
+        numba.cuda.synchronize()
+
+    if verbose:
+        print("Summing the dot products on the GPU Time: {0:.3f} s".format(stopwatch.total_run_time))
+
+
+def _get_gpu_general_matrix_matrix_product(m, n, k, a, b, distances_on_gpu, verbose):
+    """
+    Calculate the -2<a,b> cross dot products matrix and do the sum (||a||^2 + ||b||^2) -2<a,b>
+
+    :param m: First dimension of first matrix (number of samples in first matrix)
+    :type m: int
+    :param n: First dimension of second matrix (number of samples in second matrix)
+    :type n: int
+    :param k: Second dimension of both matrices (number of element in each sample)
+    :type k: int
+    :param a: First matrix
+    :type a: float32[m, k]
+    :param b: Second matrix
+    :type b: float32[n, k]
+    :param distances_on_gpu: Resulting distance matrix
+    :type distances_on_gpu: float32[m, n]
+    :param verbose: If true print the time the function took to complete
+    :type verbose: bool
+    """
+    with sw.Stopwatch() as stopwatch:
+        da = cuda.to_device(np.asfortranarray(a))
+        db = cuda.to_device(np.asfortranarray(b))
+        blas = cublas.Blas()
+        blas.gemm('N', 'T', m, n, k, -2.0, da, db, 1.0, distances_on_gpu)
+        numba.cuda.synchronize()
+
+    if verbose:
+        print("cuBLAS gemm Time: {0:.3f} s".format(stopwatch.total_run_time))
+
+
+def _calculate_distances_on_gpu(a, b, distances_on_gpu, verbose=False):
+    """
+    Calculate the pairwise distances between all row vectors of matrix a and all row vectors of matrix b (on the gpu)
+
+    :param a: First matrix
+    :type a: float32[m, k]
+    :param b: Second matrix
+    :type b: float32[n, k]
+    :param distances_on_gpu: The resulting distances between m vectors of matrix one and n vectors of matrix two
+    :type distances_on_gpu: device float32[m, n]
+    :param verbose: If true print out the times of all steps in this function
+    :type verbose: bool
+    """
+    m = a.shape[0]
+    n = b.shape[0]
+    k = a.shape[1]
+
+    dots_a, dots_b = _get_inner_products(a, b, verbose)
+
+    _get_sum_of_dot_products(dots_a, dots_b, distances_on_gpu, verbose)
+
+    _get_gpu_general_matrix_matrix_product(m, n, k, a, b, distances_on_gpu, verbose)
+
+
+def _segment_sort_transposed_distances_get_knns(num_of_neighbours, distances_on_gpu, number_of_sorts,
+                                                verbose=False):
+    """
+    Sorts the distances_on_gpu matrix (on the gpu) and for each element returns the num_of_neighbours smallest
+    distances with their position on the vector of elements (indices)
+
+    :param num_of_neighbours: Number of nearest neighbours to each element to return
+    :type num_of_neighbours: int
+    :param distances_on_gpu: The full, unsorted, pairwise elements distances
+    :type distances_on_gpu: device float32[m , m]
+    :param number_of_sorts: Given the GPU's memory, in how many pieces must the algorithm break the given distance matrix and sort each piece
+    :type number_of_sorts: int
+    :param verbose: If true print some info
+    :type verbose: bool
+    :return: The num_of_neighbors nearest elements' indices and distances vectors
+    :rtype: float32[m, k], float32[m, k]
+    """
+    m = distances_on_gpu.shape[0]  # all spikes
+    n = distances_on_gpu.shape[1]  # part of spikes in this distances_on_gpu matrix
+
+    selected_sorted_distances = np.empty((n, num_of_neighbours))
+    selected_sorted_indices = np.empty((n, num_of_neighbours))
+
+    with sw.Stopwatch() as stopwatch:
+        # Create an array with the indices each iteration will step through
+        p = np.append(np.arange(0, n, int(n / number_of_sorts)), n)
+        for i in np.arange(1, p.shape[0]):
+            # Get how many elemnts each iteration will do
+            delta_n = p[i] - p[i - 1]
+
+            # Prepare for sorting in this iteration
+            # 1. Get this iteration's keys (distances) onto the host from the gpu and reshape them appropriately
+            keys = np.ascontiguousarray(distances_on_gpu.copy_to_host()[:, p[i - 1]:p[i]].transpose().reshape(m * delta_n))
+            # 2. Create the values (indices) appropriately reshaped
+            values = np.ascontiguousarray(np.tile(np.arange(m), (delta_n, 1)).reshape(m * delta_n))
+            # 3. Define the segments
+            segments = np.ascontiguousarray(np.arange(m, m * delta_n, m))
+            # 4. Do the segmented sort
+            sorting.segmented_sort(keys=keys, vals=values, segments=segments)
+
+            # Put the smallest num_of_neighbours distances (and indices) in the selected_sorted_distances
+            # (selected_sorted_indices)
+            keys = np.reshape(keys, (delta_n, m))[:, :num_of_neighbours]
+            values = np.reshape(values, (delta_n, m))[:, :num_of_neighbours]
+            selected_sorted_distances[p[i - 1]:p[i], :] = keys[:, :]
+            selected_sorted_indices[p[i - 1]:p[i], :] = values[:, :]
+            if verbose:
+                print('     Sorted {} of {} segments of this iteration'.format(i, p.shape[0] - 1))
+
+    print("SORTING TIME: {0:.3f} s".format(stopwatch.total_run_time))
+
+    return selected_sorted_indices, selected_sorted_distances
+
+
+def _get_required_gpu_memory():
+    """
+    Return the number of bytes available in the GPU to do all following steps
+    :return: The number of GPU bytes
+    :rtype: int
+    """
+    gpu_mem = cuda.current_context().get_memory_info()
+    available_gpu_mem = 0.5 * gpu_mem[0]
+
+    return available_gpu_mem
+
+
+def calculate_knn_distances(samples_matrix, perplexity=100, verbose=True):
+    """
+    Calculates the k (perplexity * 3 + 1) nearest neighbors of all row vectors (samples) in the sample_matrix. It uses
+    the GPU to do this so it has to take into account the available GPU memory. Given that, it does the comparison in
+    steps where the first matrix is the full samples matrix and the second matrix is a sub set of it. Each step first
+    calculates all distances between the row vectors of the first and second matrix and then sorts these distances and
+    keep the k closest ones.
+
+    :param samples_matrix: The full smaples x dimensions matrix where every sample is a row in the matrix
+    :type samples_matrix: float32[m, n]
+    :param perplexity: The definition of how many nearest neighbors to keep (k = perplexity * 3 + 1)
+    :type perplexity: int
+    :param verbose: If true the function prints out the time takes to complete the different parts of it
+    :type verbose: bool
+    :return: The num_of_neighbors nearest elements' indices and distances vectors
+    :rtype: float32[m, k], float32[m, k]
+    """
+    with sw.Stopwatch as outside_stopwatch:
+        num_of_neighbours = perplexity * 3 + 1  # That is how the original t-sne defined k nearest neighbours
+        m = samples_matrix.shape[0]  # number of samples in the samples x dimensions matrix
+
+        closest_indices = np.empty((m, num_of_neighbours))
+        closest_distances = np.empty((m, num_of_neighbours))
+
+        available_gpu_mem = _get_required_gpu_memory()
+
+        n = int(np.ceil(available_gpu_mem / (4 * m)))  # The number of samples to compare in each iteration with the full m
+        # samples (defined by the GPU's available memory)
+
+        number_of_iters = int(np.ceil(m / n))  # The number of iterations so that all m samples have their k nearest
+        # neighbours with all other m - 1 samples calculated
+
+        # Generate the pairs of star and end indices for the samples that will be included in the second matrix at every
+        # iteration
+        indices_of_second_matrices = [(i, i + n) for i in np.arange(0, number_of_iters * (n - 1), n)]
+        indices_of_second_matrices[-1] = (indices_of_second_matrices[-1][0], m)
+
+        first_matrix = np.array(samples_matrix, dtype=np.float32)  # first matrix is the full samples matrix
+
+        # Start the loop over iterations filling up the closest_indices and closest_distances matrices
+        for iteration in np.arange(number_of_iters):
+            # Fill up the second matrix with the correct part of the whole samples matrix
+            second_matrix = np.array(samples_matrix[indices_of_second_matrices[iteration][0]:
+                                                    indices_of_second_matrices[iteration][1], :],
+                                     dtype=np.float32)
+
+            with sw.Stopwatch() as inside_stopwatch:
+                if iteration != 0:
+                    del distances_on_gpu
+                cuda.current_context().deallocations.clear()
+
+                if verbose:
+                    print('LOADING UP THE GPU')
+                temp = np.array(np.zeros((m, second_matrix.shape[0]), dtype=np.float32))
+                distances_on_gpu = cuda.to_device(np.asfortranarray(temp))
+
+                if verbose:
+                    print("Loading matrix time:  {0:.3f} s".format(inside_stopwatch.time_elapsed))
+
+                if verbose:
+                    print('ITERATION NUMBER: ' + str(iteration + 1))
+
+                _calculate_distances_on_gpu(a=first_matrix, b=second_matrix, distances_on_gpu=distances_on_gpu,
+                                            verbose=verbose)
+
+                available_gpu_mem = _get_required_gpu_memory()
+
+                number_of_sorts = int(np.ceil((16 * n * m) / available_gpu_mem))  # 4 is the bytes per float32,
+                # 2 is the two arrays that need to be loaded to gpu, the other factor of 2 is probably a doubling
+                # overhead in the algorithm
+
+                if verbose:
+                    print('     Number of sorting segments = ' + str(number_of_sorts + 1))
+
+                temp_indices, temp_distances = \
+                    _segment_sort_transposed_distances_get_knns(num_of_neighbours=num_of_neighbours,
+                                                                distances_on_gpu=distances_on_gpu,
+                                                                number_of_sorts=number_of_sorts, verbose=verbose)
+
+                closest_indices[indices_of_second_matrices[iteration][0]: indices_of_second_matrices[iteration][1], :] = \
+                    np.ascontiguousarray(temp_indices)
+                closest_distances[indices_of_second_matrices[iteration][0]: indices_of_second_matrices[iteration][1], :] = \
+                    np.ascontiguousarray(temp_distances)
+                if verbose:
+                    print('FINISHED CALCULATING ' + str(iteration + 1) + ' OF ' + str(number_of_iters) +
+                          ' ITERATIONS')
+
+            if verbose:
+                print("Spend Time: {0:.3f} s".format(outside_stopwatch.time_elapsed))
+
+    return closest_indices, np.sqrt(np.abs(closest_distances))
+
---
+++ spike_positioning_on_probe.py
@@ -0,0 +1,219 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+
+def _get_relevant_channels_with_threshold(threshold, template):
+    amplitude = np.nanmax(template) - np.nanmin(template)
+    points_over_threshold = np.argwhere(template > (np.nanmax(template) - threshold * amplitude))
+    channels_over_threshold = np.unique(points_over_threshold[:, 1])
+    return channels_over_threshold
+
+
+def _get_relevant_channels_over_median_peaks(threshold, template):
+    median = np.median(np.nanmin(template, axis=0))
+    std = np.std(np.nanmin(template, axis=0))
+    points_under_median = np.argwhere(template < (median - threshold*std))
+    channels_over_threshold = np.unique(points_under_median[:, 1])
+    return channels_over_threshold
+
+
+def _normalize(L, normalizeFrom=0, normalizeTo=1):
+    '''normalize values of a list to make its min = normalizeFrom and its max = normalizeTo'''
+    vMax = max(L)
+    vMin = min(L)
+    return [(x-vMin)*(normalizeTo - normalizeFrom) / (vMax - vMin) for x in L]
+# ------------------
+
+
+def generate_probe_positions_of_spikes(base_folder, binary_data_filename, number_of_channels_in_binary_file,
+                                       used_spikes_indices=None, position_mult=2.25, threshold=0.1):
+    """
+    Generate positions (x, y coordinates) for each spike on the probe. This function assumes that the spikes were
+    generated with the kilosort algorithm so the base_folder holds all the necessary .npy arrays.
+    In order for this function to find which channels are the most relevant in each spike it looks into the spike's
+    assigned template (a channels x time points array in spike_templates.npy). It then find the minimum points of all
+    channels, takes their median and their standard deviation and for each channel creates the difference between the
+    minimum and the median. Finally it demarcates the relevant to the template channels by keeping the ones whose
+    difference is larger than a number of times (threshold) over the standard deviation.
+    It then picks the relevant channels of the spike's raw data, finds the differences between the minimum value
+    and the channel's time series median value (over time), orders the channels according to these differences and
+    assigns weights between 0 and 1 (0 for a difference of 0, 1 for a maximum difference).
+    It finally finds the x, y positions of the selected channels and adds to the position of the largest difference
+    channel the weighted average positions of the remaining selected channels
+
+    :param base_folder: the folder name into which the kilosort result .npy arrays are
+    :type base_folder: string
+    :param binary_data_filename: the name of the binary file that holds the raw data that were originally passed to kilosort
+    :type binary_data_filename: string
+    :param number_of_channels_in_binary_file: How many channels does the binary file have (this is different to the number
+    of channels that are set to active in kilosort)
+    :type number_of_channels_in_binary_file: int
+    :param used_spikes_indices: which of the spikes found by kilosort should be considered.
+    :type used_spikes_indices: int[:]
+    :param threshold: the number of times the standard deviation should be larger than the difference between a
+    channel's minimum and the median of the minima of all channels in order to demarcate the channel as
+    relevant to the spike
+
+    :type threshold: float
+    :return:  The position of each spike on the probe, the distance of eac h spike on the probe from the 0, 0 of the
+    probe, the indices of the original ordering of the spikes on the new order sorted according to their distance on
+    the probe, the distance of each spike on the probe sorted
+     :rtype: float32[len(used_spike_indices), 2], float32[len(used_spike_indices)], int[len(used_spike_indices)], float32[len(used_spike_indices)]
+
+    """
+    # Load the required data from the kilosort folder
+    channel_map = np.load(os.path.join(base_folder, 'channel_map.npy'))
+    active_channel_map = np.squeeze(channel_map, axis=1)
+    channel_positions = np.load(os.path.join(base_folder, 'channel_positions.npy'))
+
+    spike_templates = np.load(os.path.join(base_folder, r'spike_templates.npy'))
+    templates = np.load(os.path.join(base_folder, r'templates.npy'))
+
+    data_raw = np.memmap(os.path.join(base_folder, binary_data_filename),
+                         dtype=np.int16, mode='r')
+
+    number_of_timepoints_in_raw = int(data_raw.shape[0] / number_of_channels_in_binary_file)
+    data_raw_kilosorted = np.reshape(data_raw, (number_of_channels_in_binary_file, number_of_timepoints_in_raw), order='F')
+
+    spike_times = np.squeeze(np.load(os.path.join(base_folder, 'spike_times.npy')).astype(np.int))
+
+    time_points = 50
+    if used_spikes_indices is None:
+        used_spikes_indices = np.arange(0, len(spike_times))
+
+    # Run the loop over all spikes to get the positions
+    counter = 0
+    weighted_average_postions = np.empty((len(used_spikes_indices), 2))
+    spike_distance_on_probe = np.empty(len(used_spikes_indices))
+    for spike_index in np.arange(0, len(used_spikes_indices)):
+        spike_raw_data = data_raw_kilosorted[active_channel_map,
+                                             (spike_times[used_spikes_indices[spike_index]]-time_points):
+                                             (spike_times[used_spikes_indices[spike_index]]+time_points)]
+        template = templates[spike_templates[used_spikes_indices[spike_index]], :, :].squeeze()
+        relevant_channels = _get_relevant_channels_over_median_peaks(threshold, template)
+
+        spike_raw_data_median_over_time = np.median(spike_raw_data, axis=1)
+        peaks_to_median = spike_raw_data_median_over_time - spike_raw_data.min(axis=1)
+        peaks_to_median = peaks_to_median[relevant_channels]
+
+        relevant_channels_sorted = [v for (k, v) in sorted(zip(peaks_to_median, relevant_channels), reverse=True)]
+
+        peaks_to_median_sorted = sorted(peaks_to_median, reverse=True)
+        peaks_to_median_sorted.append(np.median(spike_raw_data_median_over_time[relevant_channels]))
+
+        weights = _normalize(peaks_to_median_sorted)[:-1]
+        relevant_channels_positions = channel_positions[relevant_channels_sorted]
+
+        pos_x = relevant_channels_positions[0, 0]
+        pos_y = relevant_channels_positions[0, 1]
+
+        new_pos_x = pos_x - np.mean(((pos_x - relevant_channels_positions[:, 0]) * weights)[1:])
+        new_pos_y = pos_y - np.mean(((pos_y - relevant_channels_positions[:, 1]) * weights)[1:])
+        weighted_average_postions[spike_index, :] = [new_pos_x, new_pos_y]
+        spike_distance_on_probe[spike_index] = np.sqrt(np.power(new_pos_x, 2) + np.power(new_pos_y, 2))
+
+        counter += 1
+        if counter % 5000 == 0:
+            print('Completed ' + str(counter) + ' spikes')
+    weighted_average_postions = weighted_average_postions * position_mult
+
+    # sort according to position on probe
+    spike_indices_sorted_by_probe_distance = np.array([b[0] for b in sorted(enumerate(spike_distance_on_probe),
+                                                                            key=lambda dist: dist[1])])
+    spike_distances_on_probe_sorted = np.array([b[1] for b in sorted(enumerate(spike_distance_on_probe),
+                                                                     key=lambda dist: dist[1])])
+
+    return weighted_average_postions, spike_distance_on_probe, \
+        spike_indices_sorted_by_probe_distance, spike_distances_on_probe_sorted
+
+
+def generate_probe_positions_of_templates(base_folder, threshold=0.1):
+    """
+    Generate positions (x, y coordinates) for each template found by kilosort on the probe.
+    This function assumes that the base_folder holds all the necessary .npy arrays.
+    In order for this function to find which channels are the most relevant in each template it looks into the
+    template (a channels x time points array in spike_templates.npy). It then find the minimum points of all
+    channels, takes their median and their standard deviation and for each channel creates the difference between the
+    minimum and the median. Finally it demarcates the relevant to the template channels by keeping the ones whose
+    difference is larger than a number of times (threshold) over the standard deviation.
+    It then picks the relevant channels of the spike's raw data, finds the differences between the minimum value
+    and the channel's time series median value (over time), orders the channels according to these differences and
+    assigns weights between 0 and 1 (0 for a difference of 0, 1 for a maximum difference).
+    It finally finds the x, y positions of the selected channels and adds to the position of the largest difference
+    channel the weighted average positions of the remaining selected channels
+
+    :param base_folder: the folder name into which the kilosort result .npy arrays are
+    :type base_folder: string
+    :param threshold: the number of times the standard deviation should be larger than the difference between a
+    channel's minimum and the median of the minima of all channels in order to demarcate the channel asvrelevant to the
+    spike
+
+    :type threshold: float
+    :return: weighted_average_postions : the positions of the templates on the probe
+    :rtype: weighted_average_postions : float32[len(used_spike_indices) x 2]
+
+    """
+    # Load the required data from the kilosort folder
+    channel_positions = np.load(os.path.join(base_folder, 'channel_positions.npy'))
+    templates = np.load(os.path.join(base_folder, r'templates.npy'))
+    template_markings = np.load(os.path.join(base_folder, r'template_marking.npy'))
+    templates = templates[template_markings > 0, :, :]
+
+    # Run the loop over all templates to get the positions
+    counter = 0
+    templates_positions = []
+    for template in templates:
+        relevant_channels = _get_relevant_channels_over_median_peaks(threshold, template)
+
+        template_median_over_time = np.median(template, axis=0)
+        peaks_to_median = template_median_over_time - template.min(axis=0)
+        peaks_to_median = peaks_to_median[relevant_channels]
+
+        relevant_channels_sorted = [v for (k, v) in sorted(zip(peaks_to_median, relevant_channels), reverse=True)]
+
+        peaks_to_median_sorted = sorted(peaks_to_median, reverse=True)
+        peaks_to_median_sorted.append(np.median(template_median_over_time[relevant_channels]))
+
+        weights = _normalize(peaks_to_median_sorted)[:-1]
+        relevant_channels_positions = channel_positions[relevant_channels_sorted]
+
+        pos_x = relevant_channels_positions[0, 0]
+        pos_y = relevant_channels_positions[0, 1]
+
+        new_pos_x = pos_x - np.mean(((pos_x - relevant_channels_positions[:, 0]) * weights)[1:])
+        new_pos_y = pos_y - np.mean(((pos_y - relevant_channels_positions[:, 1]) * weights)[1:])
+        templates_positions.append([new_pos_x, new_pos_y])
+        counter += 1
+        if not (counter % 100):
+            print('Completed ' + str(counter) + ' templates')
+
+    return np.array(templates_positions)
+
+
+def view_spike_positions(spike_positions, brain_regions, probe_dimensions, labels_offset=80, font_size=20):
+    """
+    Plot the spike positions as a scatter plot on a probe marked with brain regions
+
+    :param spike_positions: the x,y positions of the spikes
+    :type spike_positions: (np.array((N,2)))
+    :param brain_regions: a dictionary with keys the names of the brain regions underneath the demarcating lines and
+    values the y position on the probe of the demarcating lines
+
+    :type brain_regions: dict
+    :param probe_dimensions: the x and y limits of the probe
+    :type probe_dimensions: (np.array(2))
+
+    """
+    fig = plt.figure()
+    ax = fig.add_axes([0.08, 0.05, 0.9, 0.9])
+    ax.scatter(spike_positions[:, 0], spike_positions[:, 1], s=5)
+    ax.set_xlim(0, probe_dimensions[0])
+    ax.set_ylim(0, probe_dimensions[1])
+    ax.yaxis.set_ticks(np.arange(0, probe_dimensions[1], 100))
+    ax.tick_params(axis='y', direction='in', length=5, width=1, colors='b')
+    for region in brain_regions:
+        ax.text(2, brain_regions[region] - labels_offset, region, fontsize=font_size)
+        ax.plot([0, probe_dimensions[0]], [brain_regions[region], brain_regions[region]], 'k--', linewidth=2)
+    return fig, ax
+
