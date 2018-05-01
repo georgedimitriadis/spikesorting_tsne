0250bff5d73deb02c3a567c92c8c4a60c31d0061
georgedimitriadis <g.dimitri@hotmail.com>
2018-05-01 17:27:30 +0100

    Moved files and added files to turn this into a pypi package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spikesorting_tsne',
+      version='1.0.0',
+      description='T-sne with burnes hut and cuda extension (with python wrappers and python code for for spike sorting)',
+      url='https://github.com/georgedimitriadis/spikesorting_tsne',
+      author='George Dimitriadis',
+      author_email='gdimitri@hotmail.com',
+      license='MIT',
+      packages=['spikesorting_tsne'],
+      zip_safe=False)
